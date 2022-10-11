4c0ee13a0729c8310d54da44ede1b992b2272ccd
László Békéssy <borzos6@gmail.com>
2022-10-11 22:14:40 +0200

    Link changed to A because of external links

--- src/components/ContactPoints/ContactPoints.jsx
+++ src/components/ContactPoints/ContactPoints.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import {Link} from 'gatsby';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faFacebookSquare, faLinkedin, faGithubSquare} from '@fortawesome/free-brands-svg-icons';
 import {container} from './ContactPoints.module.scss'
@@ -7,15 +6,15 @@ import {container} from './ContactPoints.module.scss'
 const ContactPoints = () => {
   return (
     <h2 className={`is-size-3 is-muted ${container}`}>
-        <Link to="https://www.facebook.com/bekessy.laszlo" target="_blank">
+        <a href="https://www.facebook.com/bekessy.laszlo" target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faFacebookSquare} />
-        </Link>&nbsp;
-        <Link to="https://linkedin.com/in/laszlo-bekessy" target="_blank">
+        </a>&nbsp;
+        <a href="https://linkedin.com/in/laszlo-bekessy" target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faLinkedin} />
-        </Link>&nbsp;
-        <Link to="https://github.com/borzos6" target="_blank">
+        </a>&nbsp;
+        <a href="https://github.com/borzos6" target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faGithubSquare} />
-        </Link>
+        </a>
     </h2>              
   )
 }
