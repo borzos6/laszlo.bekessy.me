fece3856f4413ed5c92066b2a3b5de0a34b127d4
László Békéssy <borzos6@gmail.com>
2022-10-13 00:11:03 +0200

    Strongest tech section added, TechSection updated

---
+++ src/sections/StrongestTechStackSection/StrongestTechStackSection.jsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+
+import { Container, Columns, Content } from 'react-bulma-components';
+import { TechStackElement } from '../../components/';
+import { projectCodeBerry, projectYearCompass, projectInvisibleUniversity, projectABF } from '../../images';
+
+const StrongestTechStackSection = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allContentJson {
+                nodes {
+                strongestActualTechStack {
+                    devicon
+                    name
+                }
+                }
+            }
+        }
+    `)
+  return (
+    <Columns className='mt-4'>
+        {
+            data.allContentJson.nodes[0].strongestActualTechStack.map(node => (
+                <TechStackElement devicon={node.devicon} name={node.name}/>
+            ))
+        }
+    </Columns> 
+  )
+}
+
+export {StrongestTechStackSection};
+
+
+
+
--- src/sections/TechSection/TechSection.jsx
+++ src/sections/TechSection/TechSection.jsx
@@ -1,18 +1,29 @@
 import * as React from 'react'
-import { Container, Columns } from 'react-bulma-components';
+import { Container, Columns, Content } from 'react-bulma-components';
 import { SectionSentence } from '../../components/';
+import { StrongestTechStackSection } from '../../sections/';
 import { watercolor2Sketch } from '../../images';
 
 const TechSection = () => {
   return (
     <Container className='mt-4'>
-        <Columns className="side-feature">
+        <Columns className="side-feature" vCentered={true}>
             <Columns.Column size={6} offset={1} >
-                <h3 className="title is-3 mb-10 is-title-reveal">Handcrafted UI</h3>
-                <p className="subtitle is-5 is-muted">
-                    Fresh Landing Page comes with a nice component library based on Bulma. 
-                    Each component has been carefully handcrafted.
-                </p>
+                <h4 className="title is-size-4 mb-0 is-title-reveal">
+                    CTO, architect, engineer for <span className='has-text-primary'>{`${new Date().getFullYear()-2004}`}+ years</span>
+                </h4>
+                <Content size="small">
+                    <ul>
+                        <li><span className='has-text-weight-bold'>Full-stack generalist</span>, touched and worked with every aspect of software engineering</li>
+                        <li>Specialist in <span className='has-text-weight-bold'>vertical integration. </span>From infrastructure to customer experience</li>
+                        <li>Wrote 5 <span className='has-text-weight-bold'>self-generating software</span> for different business cases</li>
+                        <li>Tech lead with human touch – focuses on <span className='has-text-weight-bold'>people-first management </span> as a leader</li>
+                    </ul>
+                </Content>
+                <h4 className="title mb-0 is-size-5">
+                    My actual strongest stack
+                </h4>
+                <StrongestTechStackSection/>
             </Columns.Column>
             <Columns.Column size={4}>
                 <SectionSentence
@@ -22,6 +33,9 @@ const TechSection = () => {
                 />
             </Columns.Column>
         </Columns>
+
+        
+
     </Container>
   )
 }
