a3f9cdc4cb7675c5ebaf8c5597714a187bbde8e3
László Békéssy <borzos6@gmail.com>
2022-10-12 15:04:20 +0200

    Integration. LeadSection extended with new components and content

--- src/images/index.js
+++ src/images/index.js
@@ -1,4 +1,7 @@
+// Profile
 export { default as profilePicture } from "./profile/profile.png";
+
+// Sketches
 export { default as officeSketch } from "./sketches/office-sketch.png";
 export { default as treeWatercolorSketch } from "./sketches/tree-watercolor.png";
 export { default as globe1Sketch } from "./sketches/globe-1.webp";
@@ -6,5 +9,11 @@ export { default as globe2Sketch } from "./sketches/globe-2.webp";
 export { default as watercolor1Sketch } from "./sketches/watercolor-1.png";
 export { default as watercolor2Sketch } from "./sketches/watercolor-2.png";
 export { default as watercolor3Sketch } from "./sketches/watercolor-3.png";
+export { default as watercolor3BSketch } from "./sketches/watercolor-3B.png";
 export { default as watercolor4Sketch } from "./sketches/watercolor-4.png";
-export { default as watercolor5Sketch } from "./sketches/watercolor-5.png";
+
+// Projects
+export { default as projectCodeBerry } from "./projects/codeberry.png";
+export { default as projectYearCompass } from "./projects/yearcompass.png";
+export { default as projectInvisibleUniversity } from "./projects/invisible-university.png";
+export { default as projectABF } from "./projects/abf.png";
--- src/sections/LeadSection/LeadSection.jsx
+++ src/sections/LeadSection/LeadSection.jsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
-import { Container, Columns } from 'react-bulma-components';
+import { Container, Columns, Content } from 'react-bulma-components';
 import { SectionSentence } from '../../components/';
 import { watercolor1Sketch } from '../../images';
+import { ProjectCardsSection } from '../../sections';
 
 const LeadSection = () => {
   return (
     <Container className='mt-4'>
-        <Columns className="side-feature">
+        <Columns className="side-feature" vCentered={true}>
             <Columns.Column size={4} offset={1}>
                 <SectionSentence
                     sentence="I lead teams"
@@ -15,14 +16,21 @@ const LeadSection = () => {
                 />
             </Columns.Column>
             <Columns.Column size={6}>
-                <h3 className="title is-3 mb-10 is-title-reveal">Handcrafted UI</h3>
-                <p className="subtitle is-5 is-muted">
-                    Fresh Landing Page comes with a nice component library based on Bulma. 
-                    Each component has been carefully handcrafted.
-                </p>
+                <h3 className="title is-size-4 mb-0 is-title-reveal">
+                    <span className='has-text-danger'>{`${new Date().getFullYear()-2008}`} years</span> in leadership
+                </h3>
+                <Content size="small">
+                    <ul>
+                        <li>Leading teams 4 to 250 <span className='has-text-weight-bold'>since 2008</span></li>
+                        <li><span className='has-text-weight-bold'>Founder</span> of multiple non-profits (9y), SMBs (2y) and Startups (7y)</li>
+                        <li>Deep motivation and practical experience in building <span className='has-text-weight-bold'>diverse, multi-language, multi-culture teams</span> (8y)</li>
+                        <li>Serial entrepreneur from heart, founded & validated <span className='has-text-weight-bold'>more than 30 projects</span>.</li>
+                    </ul>
+                </Content>
             </Columns.Column>
-            
         </Columns>
+
+        <ProjectCardsSection/>
     </Container>
   )
 }
