fe7f758fd494f3b28dbca7f1123b309502287376
László Békéssy <borzos6@gmail.com>
2022-10-12 04:45:13 +0200

    Facebook removed from contactpoints

--- src/components/ContactPoints/ContactPoints.jsx
+++ src/components/ContactPoints/ContactPoints.jsx
@@ -1,14 +1,11 @@
 import * as React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faFacebookSquare, faLinkedin, faGithubSquare} from '@fortawesome/free-brands-svg-icons';
+import {faLinkedin, faGithubSquare} from '@fortawesome/free-brands-svg-icons';
 import {container} from './ContactPoints.module.scss'
 
 const ContactPoints = () => {
   return (
     <h2 className={`is-size-3 is-muted ${container}`}>
-        <a href="https://www.facebook.com/bekessy.laszlo" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faFacebookSquare} />
-        </a>&nbsp;
         <a href="https://linkedin.com/in/laszlo-bekessy" target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faLinkedin} />
         </a>&nbsp;
