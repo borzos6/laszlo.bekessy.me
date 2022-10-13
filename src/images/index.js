775b2d75dfbec13f8d9401a25f706b8a76b4ec59
László Békéssy <borzos6@gmail.com>
2022-10-13 13:35:41 +0200

    Asset cleanup

--- src/images/index.js
+++ src/images/index.js
@@ -2,15 +2,11 @@
 export { default as profilePicture } from "./profile/profile.png";
 
 // Sketches
-export { default as officeSketch } from "./sketches/office-sketch.png";
-export { default as treeWatercolorSketch } from "./sketches/tree-watercolor.png";
 export { default as globe1Sketch } from "./sketches/globe-1.webp";
 export { default as globe2Sketch } from "./sketches/globe-2.webp";
 export { default as watercolor1Sketch } from "./sketches/watercolor-1.png";
 export { default as watercolor2Sketch } from "./sketches/watercolor-2.png";
 export { default as watercolor3Sketch } from "./sketches/watercolor-3.png";
-export { default as watercolor3BSketch } from "./sketches/watercolor-3B.png";
-export { default as watercolor4Sketch } from "./sketches/watercolor-4.png";
 
 // Projects
 export { default as projectCodeBerry } from "./projects/codeberry.png";
