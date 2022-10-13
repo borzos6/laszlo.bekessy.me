3ef81f3dbd6afa68518abd298684c9be7b1b603f
László Békéssy <borzos6@gmail.com>
2022-10-13 13:35:30 +0200

    "Lead@ section adjusted a bit, added animations, redesigned spacing and things like that

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ export {ContactPoints} from './ContactPoints/ContactPoints';
 export {Divider} from './Divider/Divider';
 export {IntroPicture} from './IntroPicture/IntroPicture';
 export {Layout} from './Layout/Layout';
-export {ProjectCard} from './ProjectCard/ProjectCard';
+export {ProjectCard} from './ProjectCard';
 export {SectionSentence} from './SectionSentence/SectionSentence';
 export {TechStackElement} from './TechStackElement/TechStackElement';
--- src/sections/LeadSection/LeadSection.jsx
+++ src/sections/LeadSection/LeadSection.jsx
@@ -16,15 +16,15 @@ const LeadSection = () => {
                 />
             </Columns.Column>
             <Columns.Column size={6}>
-                <h3 className="title is-size-4 mb-0 is-title-reveal">
+                <h3 className="title is-size-3 mb-0 is-title-reveal">
                     <span className='has-text-danger'>{`${new Date().getFullYear()-2008}`} years</span> in leadership
                 </h3>
-                <Content size="small">
+                <Content size="">
                     <ul>
-                        <li>Leading teams 4 to 250 <span className='has-text-weight-bold'>since 2008</span></li>
-                        <li><span className='has-text-weight-bold'>Founder</span> of multiple non-profits (9y), SMBs (2y) and Startups (7y)</li>
-                        <li>Deep motivation and practical experience in building <span className='has-text-weight-bold'>diverse, multi-language, multi-culture teams</span> (8y)</li>
-                        <li>Serial entrepreneur from heart, founded & validated <span className='has-text-weight-bold'>more than 30 projects</span>.</li>
+                        <li>Leading teams of 4 to 250 <span className='has-text-weight-bold'>since 2008</span></li>
+                        <li><span className='has-text-weight-bold'>Founder</span> of multiple non-profits (9y), SMBs (3y) and Startups (7y)</li>
+                        <li>Practical experience in building <span className='has-text-weight-bold'>diverse, multi-language, multi-culture teams</span> (8y)</li>
+                        <li>Serial entrepreneur from heart, founded and validated <span className='has-text-weight-bold'>more than 30 projects</span></li>
                     </ul>
                 </Content>
             </Columns.Column>
