ac9163d8b01076840e308ce48634ccde0017f436
László Békéssy <borzos6@gmail.com>
2022-10-13 18:13:06 +0200

    small cleanup

--- src/sections/StrongestTechStackSection/StrongestTechStackSection.jsx
+++ src/sections/StrongestTechStackSection/StrongestTechStackSection.jsx
@@ -1,9 +1,7 @@
 import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
-
-import { Container, Columns, Content } from 'react-bulma-components';
+import { useStaticQuery, graphql } from 'gatsby'
+import { Columns } from 'react-bulma-components';
 import { TechStackElement } from '../../components/';
-import { projectCodeBerry, projectYearCompass, projectInvisibleUniversity, projectABF } from '../../images';
 
 const StrongestTechStackSection = () => {
     const data = useStaticQuery(graphql`
