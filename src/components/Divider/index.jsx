71441aa5f4160d55b8accaaa014a48c40ec2da39
László Békéssy <borzos6@gmail.com>
2022-10-15 17:11:39 +0200

    Divider made responsive, duplicate component deleted

--- src/components/Divider/index.jsx
+++ src/components/Divider/index.jsx
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import * as syles from './styles.module.scss';
 
 const Divider = (props) => {
-    return <div className={`${syles.divider} ${props.text ? syles.withText : ''}`}>{props.text}</div>;
+    return (
+        <div className={`${syles.divider} ${props.text ? syles.withText : ''}`}>
+            <span className="has-text-centered">{props.text}</span>
+        </div>
+    );
 };
 
 Divider.propTypes = {
